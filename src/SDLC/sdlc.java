94e92c7d8c7d126b5d1f3094f3ba3392c0d4fa8d
jlucey01 <jlucey@icloud.com>
2023-01-30 10:13:21 -0500

    Update sdlc.java

--- src/SDLC/sdlc.java
+++ src/SDLC/sdlc.java
@@ -5,21 +5,29 @@ import java.net.URL;
 import java.util.Scanner;
 public class sdlc {
    public static void main(String args[]) throws IOException {
-      //Instantiating the URL class
+	   
+      //Grabbing the URL
+	   
       URL url = new URL("https://www.gutenberg.org/files/1065/1065-h/1065-h.htm");
-      //Retrieving the contents of the specified page
+      
+      //Reading the page
+      
       Scanner sc = new Scanner(url.openStream());
-      //Instantiating the StringBuffer class to hold the result
+      
+      //Start StringBuffer to hold the file
+      
       StringBuffer sb = new StringBuffer();
       while(sc.hasNext()) {
          sb.append(sc.next());
-         //System.out.println(sc.next());
+         
       }
-      //Retrieving the String from the String Buffer object
+      //Grabbing the String
+      
       String result = sb.toString();
       System.out.println(result);
+      
       //Removing the HTML tags
-      result = result.replaceAll("<[^>]*>", "");
+      
       System.out.println("Contents of the web page: "+result);
       System.out.println("Finally Finished Reading!");
    }

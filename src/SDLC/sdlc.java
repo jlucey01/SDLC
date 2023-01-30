6aa5b20b0347e190419d6ea362e7d89db453ea4f
jlucey01 <jlucey@icloud.com>
2023-01-30 10:33:28 -0500

    Update sdlc.java

--- src/SDLC/sdlc.java
+++ src/SDLC/sdlc.java
@@ -3,6 +3,8 @@ package SDLC;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
+
+
 public class sdlc {
    public static void main(String args[]) throws IOException {
 	   
@@ -16,17 +18,19 @@ public class sdlc {
       
       //Start StringBuffer to hold the file
       
-      StringBuffer sb = new StringBuffer();
+      StringBuffer strbuff = new StringBuffer();
       while(scnr.hasNext()) {
-         sb.append(scnr.next());
+         strbuff.append(scnr.next());
       }
       
       //Grabbing the String
       
-      String result = sb.toString();
+      String result = strbuff.toString();
       System.out.println(result);
       
       //Removing the HTML tags
+      result = result.replaceAll("<.*?>", "");
+      result = result.replaceAll("&.*?;" , "");
       
       System.out.println("Contents of the web page: "+result);
       System.out.println("Finally Finished Reading File!");

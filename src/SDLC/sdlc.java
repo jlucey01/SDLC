b6ae52289c1f4d4ce1f9268eb730e162ab821683
jlucey01 <jlucey@icloud.com>
2023-02-06 20:53:20 -0500

    Update sdlc.java

--- src/SDLC/sdlc.java
+++ src/SDLC/sdlc.java
@@ -44,33 +44,6 @@ public class sdlc {
 		}
     	  words.entrySet().forEach(System.out::println);
     	  
-    	  
-    	  
-    	  /*System.out.println(words);
-    	  /*System.out.println("\r\n");
-      
-      
-      
-      //Start StringBuffer to hold the file
-      
-      /*StringBuffer strbuff = new StringBuffer();
-      while(scnr.hasNext()) {
-         strbuff.append(scnr.next());
-      }
-      
-      //Grabbing the String
-      
-      String result = strbuff.toString();
-      System.out.println(result);
-      
-      //Removing the HTML tags
-      
-      result = result.replaceAll("\\<[\s]*tag[^>]*>"," ");
-      result = result.replaceAll("&.*?;" , " ");
-      
-      System.out.println("Contents of the web page: " + result);
-      System.out.println("Finally Finished Reading File!");*/
-      
       
    }
 	    
